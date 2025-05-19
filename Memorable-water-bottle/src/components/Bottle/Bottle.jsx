48f6e4be8d4772c86b28871971b17ea1f420478a
ApurboShib <shibjoy088@gmail.com>
2025-05-19 17:32:48 +0600

    make it more readable and add useful comment and also add style

--- Memorable-water-bottle/src/components/Bottle/Bottle.jsx
+++ Memorable-water-bottle/src/components/Bottle/Bottle.jsx
@@ -1,16 +1,21 @@
-import "./Bottle.css";
+import "./Bottle.css"; 
 
 const Bottle = ({ bottle, HandleAddToCart }) => {
-  //   console.log(bottle);
-  // make it distructured.
-
+  // Destructuring the bottle object to extract name, img, and price properties
   const { name, img, price } = bottle;
+
   return (
     <div className="bottle">
+      {/* Displaying the name of the bottle */}
       <h2>{name}</h2>
 
+      {/* Displaying the image of the bottle */}
       <img src={img} alt="" />
+
+      {/* Displaying the price of the bottle */}
       <p>price : ${price}</p>
+
+      {/* Button to handle adding the bottle to the cart */}
       <button onClick={() => HandleAddToCart(bottle)}>Purchese</button>
     </div>
   );
--- Memorable-water-bottle/src/components/Bottles/Bottles.jsx
+++ Memorable-water-bottle/src/components/Bottles/Bottles.jsx
@@ -2,35 +2,38 @@ import { useEffect, useState } from "react";
 import Bottle from "../Bottle/Bottle";
 import "./Bottles.css";
 const Bottles = () => {
+  // Step 1: create state to store bottle data
   const [bottles, setBottles] = useState([]);
 
-    const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  // Step 2: use useEffect to load data when component mounts
   useEffect(() => {
     fetch("bottles.json")
       .then((res) => res.json())
       //   .then((data) => console.log(data))
       .then((data) => setBottles(data));
   }, []);
-    
-    // for cards
-    const HandleAddToCart = (bottle) => {
-        // console.log("bottle are going to be added", bottle);
-        const newCart = [...cart, bottle];
-        setCart(newCart);
 
-    }
+  // for cards
+  const HandleAddToCart = (bottle) => {
+    // console.log("bottle are going to be added", bottle);
+    const newCart = [...cart, bottle];
+    setCart(newCart);
+  };
   return (
     <div>
-          <h2>Total Bottle : {bottles.length} </h2>
-          <h4>Cart : {cart.length} </h4>
+      {/* Step 6: show how many bottles were loaded */}
+      <h2>Total Bottle : {bottles.length} </h2>
+      <h4 className="cart">Cart : {cart.length} </h4>
       <div className="bottle-container">
-              {
-            bottles.map((bottle) => (
-            <Bottle
-              bottle={bottle}
-              key={bottle.id}
-              HandleAddToCart={HandleAddToCart}
-            />
+        {/* Step 7: loop through bottles and show them */}
+        {bottles.map((bottle) => (
+          <Bottle
+            bottle={bottle}
+            key={bottle.id}
+            HandleAddToCart={HandleAddToCart}
+          />
         ))}
       </div>
     </div>
